ac0c6695cdb26607f3e6c1c87bb446de7f74acf2
ACgay_caoyue <33208541+KIKOcaoyue@users.noreply.github.com>
2020-08-11 21:12:49 +0800

    Add files via upload

---
+++ DanciNotebook/DanciNotebook/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace DanciNotebook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.word = new System.Windows.Forms.Label();
+            this.translate = new System.Windows.Forms.Label();
+            this.example = new System.Windows.Forms.Label();
+            this.knownbtn = new System.Windows.Forms.Button();
+            this.unknownbtn = new System.Windows.Forms.Button();
+            this.Label_1 = new System.Windows.Forms.Label();
+            this.grade = new System.Windows.Forms.Label();
+            this.newword = new System.Windows.Forms.TextBox();
+            this.newtranslate = new System.Windows.Forms.TextBox();
+            this.newexample = new System.Windows.Forms.TextBox();
+            this.addbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.savebtn = new System.Windows.Forms.Button();
+            this.showbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // word
+            // 
+            this.word.AutoSize = true;
+            this.word.Location = new System.Drawing.Point(366, 75);
+            this.word.Name = "word";
+            this.word.Size = new System.Drawing.Size(55, 15);
+            this.word.TabIndex = 0;
+            this.word.Text = "label1";
+            // 
+            // translate
+            // 
+            this.translate.AutoSize = true;
+            this.translate.Location = new System.Drawing.Point(366, 137);
+            this.translate.Name = "translate";
+            this.translate.Size = new System.Drawing.Size(55, 15);
+            this.translate.TabIndex = 1;
+            this.translate.Text = "label2";
+            // 
+            // example
+            // 
+            this.example.AutoSize = true;
+            this.example.Location = new System.Drawing.Point(366, 201);
+            this.example.Name = "example";
+            this.example.Size = new System.Drawing.Size(55, 15);
+            this.example.TabIndex = 2;
+            this.example.Text = "label3";
+            // 
+            // knownbtn
+            // 
+            this.knownbtn.Location = new System.Drawing.Point(260, 260);
+            this.knownbtn.Name = "knownbtn";
+            this.knownbtn.Size = new System.Drawing.Size(75, 29);
+            this.knownbtn.TabIndex = 3;
+            this.knownbtn.Text = "我会了";
+            this.knownbtn.UseVisualStyleBackColor = true;
+            this.knownbtn.Click += new System.EventHandler(this.knownbtn_Click);
+            // 
+            // unknownbtn
+            // 
+            this.unknownbtn.Location = new System.Drawing.Point(458, 260);
+            this.unknownbtn.Name = "unknownbtn";
+            this.unknownbtn.Size = new System.Drawing.Size(75, 29);
+            this.unknownbtn.TabIndex = 4;
+            this.unknownbtn.Text = "我不会";
+            this.unknownbtn.UseVisualStyleBackColor = true;
+            this.unknownbtn.Click += new System.EventHandler(this.unknownbtn_Click);
+            // 
+            // Label_1
+            // 
+            this.Label_1.AutoSize = true;
+            this.Label_1.Location = new System.Drawing.Point(47, 75);
+            this.Label_1.Name = "Label_1";
+            this.Label_1.Size = new System.Drawing.Size(143, 15);
+            this.Label_1.TabIndex = 5;
+            this.Label_1.Text = "掌握度(越高越差)：";
+            // 
+            // grade
+            // 
+            this.grade.AutoSize = true;
+            this.grade.Location = new System.Drawing.Point(187, 75);
+            this.grade.Name = "grade";
+            this.grade.Size = new System.Drawing.Size(55, 15);
+            this.grade.TabIndex = 6;
+            this.grade.Text = "label1";
+            // 
+            // newword
+            // 
+            this.newword.Location = new System.Drawing.Point(50, 323);
+            this.newword.Name = "newword";
+            this.newword.Size = new System.Drawing.Size(100, 25);
+            this.newword.TabIndex = 7;
+            // 
+            // newtranslate
+            // 
+            this.newtranslate.Location = new System.Drawing.Point(174, 323);
+            this.newtranslate.Name = "newtranslate";
+            this.newtranslate.Size = new System.Drawing.Size(100, 25);
+            this.newtranslate.TabIndex = 8;
+            // 
+            // newexample
+            // 
+            this.newexample.Location = new System.Drawing.Point(297, 323);
+            this.newexample.Name = "newexample";
+            this.newexample.Size = new System.Drawing.Size(463, 25);
+            this.newexample.TabIndex = 9;
+            // 
+            // addbtn
+            // 
+            this.addbtn.Location = new System.Drawing.Point(348, 382);
+            this.addbtn.Name = "addbtn";
+            this.addbtn.Size = new System.Drawing.Size(91, 29);
+            this.addbtn.TabIndex = 10;
+            this.addbtn.Text = "添加新单词";
+            this.addbtn.UseVisualStyleBackColor = true;
+            this.addbtn.Click += new System.EventHandler(this.addbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 305);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "新单词";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 305);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "释义";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(294, 305);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "例句";
+            // 
+            // savebtn
+            // 
+            this.savebtn.Location = new System.Drawing.Point(713, 12);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(75, 23);
+            this.savebtn.TabIndex = 14;
+            this.savebtn.Text = "保存";
+            this.savebtn.UseVisualStyleBackColor = true;
+            this.savebtn.Click += new System.EventHandler(this.savebtn_Click);
+            // 
+            // showbtn
+            // 
+            this.showbtn.Location = new System.Drawing.Point(685, 260);
+            this.showbtn.Name = "showbtn";
+            this.showbtn.Size = new System.Drawing.Size(75, 23);
+            this.showbtn.TabIndex = 15;
+            this.showbtn.Text = "显示释义";
+            this.showbtn.UseVisualStyleBackColor = true;
+            this.showbtn.Click += new System.EventHandler(this.showbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showbtn);
+            this.Controls.Add(this.savebtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addbtn);
+            this.Controls.Add(this.newexample);
+            this.Controls.Add(this.newtranslate);
+            this.Controls.Add(this.newword);
+            this.Controls.Add(this.grade);
+            this.Controls.Add(this.Label_1);
+            this.Controls.Add(this.unknownbtn);
+            this.Controls.Add(this.knownbtn);
+            this.Controls.Add(this.example);
+            this.Controls.Add(this.translate);
+            this.Controls.Add(this.word);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label word;
+        private System.Windows.Forms.Label translate;
+        private System.Windows.Forms.Label example;
+        private System.Windows.Forms.Button knownbtn;
+        private System.Windows.Forms.Button unknownbtn;
+        private System.Windows.Forms.Label Label_1;
+        private System.Windows.Forms.Label grade;
+        private System.Windows.Forms.TextBox newword;
+        private System.Windows.Forms.TextBox newtranslate;
+        private System.Windows.Forms.TextBox newexample;
+        private System.Windows.Forms.Button addbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button savebtn;
+        private System.Windows.Forms.Button showbtn;
+    }
+}
+
---
+++ DanciNotebook/DanciNotebook/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DanciNotebook
+{
+    public partial class Form1 : Form
+    {
+        public class Word
+        {
+            private string word;
+            private string translate;
+            private string example;
+            private int    grade;
+
+            public Word(){ }
+            public Word(string word, string translate, string example, int grade)
+            {
+                this.word = word;
+                this.translate = translate;
+                this.example = example;
+                this.grade = grade;
+            }
+            public void setWord(string word)
+            {
+                this.word = word;
+            }
+            public void setTranslate(string translate)
+            {
+                this.translate = translate;
+            }
+            public void setExample(string example)
+            {
+                this.example = example;
+            }
+            public void setGrade(int grade)
+            {
+                this.grade = grade;
+            }
+            
+            public string getWord()
+            {
+                return this.word;
+            }
+            public string getTranslate()
+            {
+                return this.translate;
+            }
+            public string getExample()
+            {
+                return this.example;
+            }
+            public int getGrade()
+            {
+                return this.grade;
+            }
+        }
+
+        List<Word> words = new List<Word>();
+        List<Word> wordlist = new List<Word>();
+        int now = 0;
+
+        public void shuffe()
+        {
+            Random rd = new Random();
+            while(words.Count > 0)
+            {
+                int selector = rd.Next(0, words.Count);
+                wordlist.Add(words[selector]);
+                words.RemoveAt(selector);
+            }
+            for(int i=0;i<wordlist.Count;i++)
+            {
+                Console.WriteLine(wordlist[i].getWord());
+            }
+        }
+
+        public void displayword(int now)
+        {
+            word.Text = wordlist[now].getWord();
+            grade.Text = wordlist[now].getGrade().ToString();
+        }
+
+        public void displaytranslate(int now)
+        {
+            translate.Text = wordlist[now].getTranslate();
+            example.Text = wordlist[now].getExample();
+        }
+
+        public int findWord(string val)
+        {
+            for(int i=0;i<wordlist.Count;i++)
+            {
+                if (wordlist[i].getWord().Equals(val))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                using (StreamReader srd = new StreamReader("dancidata.txt"))
+                {
+                    int cnt = 0;
+                    string line;
+                    Word word = new Word();
+                    while ((line = srd.ReadLine()) != null)
+                    {
+                        cnt++;
+                        if(cnt%4==1)
+                        {
+                            word.setWord(line);
+                        }
+                        else if(cnt%4==2)
+                        {
+                            word.setTranslate(line);
+                        }
+                        else if(cnt%4==3)
+                        {
+                            word.setExample(line);
+                        }
+                        else if(cnt%4==0)
+                        {
+                            word.setGrade(int.Parse(line));
+                        }
+                        if (cnt == 4)
+                        {
+                            cnt = 0;
+                            words.Add(word);
+                            word = new Word();
+                        }
+                    }
+                }
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            shuffe();
+            if(wordlist.Count>0)
+            {
+                displayword(now);
+            }
+            else
+            {
+                word.Text = "还没有单词";
+            }
+        }
+
+        private void knownbtn_Click(object sender, EventArgs e)
+        {
+            int now = findWord(word.Text);
+            int nowgrade = wordlist[now].getGrade();
+            wordlist[now].setGrade(--nowgrade);
+            if(nowgrade==0)
+            {
+                wordlist.RemoveAt(now);
+                displayword(now);
+                translate.Text = "遮挡住了";
+                example.Text = "遮挡住了";
+            }
+            else
+            {
+                if(now<wordlist.Count-1)
+                {
+                    displayword(now+1);
+                    translate.Text = "遮挡住了";
+                    example.Text = "遮挡住了";
+                }
+            }
+        }
+
+        private void unknownbtn_Click(object sender, EventArgs e)
+        {
+            int now = findWord(word.Text);
+            int nowgrade = wordlist[now].getGrade();
+            wordlist[now].setGrade(++nowgrade);
+            if (now < wordlist.Count-1)
+            {
+                displayword(now+1);
+                translate.Text = "遮挡住了";
+                example.Text = "遮挡住了";
+            }
+        }
+
+        private void addbtn_Click(object sender, EventArgs e)
+        {
+            Word neword = new Word(newword.Text, newtranslate.Text, newexample.Text, 5);
+            wordlist.Add(neword);
+            newword.Text = "";
+            newtranslate.Text = "";
+            newexample.Text = "";
+        }
+
+        private void showbtn_Click(object sender, EventArgs e)
+        {
+            int now = findWord(word.Text);
+            displaytranslate(now);
+        }
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            System.IO.File.WriteAllText("dancidata.txt", string.Empty);
+            using (StreamWriter sw = new StreamWriter("dancidata.txt"))
+            {
+                for(int i=0;i<wordlist.Count;i++)
+                {
+                    sw.WriteLine(wordlist[i].getWord());
+                    sw.WriteLine(wordlist[i].getTranslate());
+                    sw.WriteLine(wordlist[i].getExample());
+                    sw.WriteLine(wordlist[i].getGrade().ToString());
+                }
+            }
+        }
+    }
+}
